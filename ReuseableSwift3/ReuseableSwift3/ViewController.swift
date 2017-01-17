04ed2d39c20398199004760bf56e9a8f3730a1b4
Tho <nguyenductho89@gmail.com>
2017-01-17 22:59:30 +0700

    Init

---
+++ ReuseableSwift3/ReuseableSwift3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ReuseableSwift3
+//
+//  Created by Tho on 1/17/17.
+//  Copyright © 2017 Tho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
